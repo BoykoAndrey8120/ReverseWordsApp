7f206d12343d731fb22bb9853479419a49dae83a
Andrey <boyko8120@gmail.com>
2021-10-24 13:21:44 +0300

    Added new files

---
+++ ReversWordTest/ClassOfString.swift
@@ -0,0 +1,8 @@
+//
+//  ClassOfString.swift
+//  ReversWordTest
+//
+//  Created by Andrey Boyko on 26.09.2021.
+//
+
+import Foundation
---
+++ ReversWordTest/Helpers.swift
@@ -0,0 +1,8 @@
+//
+//  Helpers.swift
+//  ReversWordTest
+//
+//  Created by Andrey Boyko on 04.10.2021.
+//
+
+import Foundation
